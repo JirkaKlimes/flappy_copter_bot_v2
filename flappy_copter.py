0b33f2968ceea858d16f2ef4a58ccab24f836e88
JirkaKlimes <jiriklimes920@gmail.com>
2022-03-31 12:02:44 +0200

    update

--- flappy_copter.py
+++ flappy_copter.py
@@ -2,6 +2,8 @@ import pygame as pg
 from keyboard import is_pressed
 import random
 import time
+from math import sqrt
+pg.init()
 
 
 
@@ -13,7 +15,7 @@ class Game:
         SIZE = 18
         COLOR = (252, 239, 61)
 
-        JUMP_STRENGTH = [3, 18]
+        JUMP_STRENGTH = [4, 20]
 
         def __init__(self, game):
             self.vel = [0, 0]
@@ -21,8 +23,17 @@ class Game:
             self.game = game
             self.dead = False
             self.last_jump = False
-
-        def update(self, jump=False, ofset=0):
+            self.jump = 0
+            self.score = 0
+            self.waiting_score_reset = False
+        
+        def set_jump(self, jump):
+            self.jump = jump
+ 
+        def update(self, jump=False, ofset=0, new_pipes=False):
+            jump = self.jump
+            if self.dead: return ofset
+            if new_pipes: self.waiting_score_reset = False
             j = jump
             jump = False if jump == self.last_jump else jump
             self.last_jump = j
@@ -52,10 +63,19 @@ class Game:
             if self.pos[1]+self.SIZE/2 > self.game.RESOLUTION[1]:
                 self.dead = True
             
+            for block in self.game.blocks.blocks:
+                dist = sqrt(pow((self.pos[0]-block[0]), 2)+pow((self.pos[1]-block[1]),2))
+                if dist < self.game.blocks.SIZE/2 + self.SIZE/2:
+                    self.dead = True
+
+            if not self.dead and not self.waiting_score_reset:
+                if self.pos[1] < self.game.pipes.lower_pipes_pos[1]:
+                    self.score += 1
+                    self.waiting_score_reset = True
+            
             ofset = self.game.TOP_BOUND - self.pos[1] if self.pos[1] < self.game.TOP_BOUND else 0
 
             pg.draw.circle(self.game.screen, self.COLOR, self.pos, self.SIZE // 2)
-
             return ofset
 
     class Pipes:
@@ -97,24 +117,60 @@ class Game:
             if self.lower_pipes_pos[1] > self.game.RESOLUTION[1]+self.PIPE_WIDTH//2:
                 self.lower_pipes_pos = self.upper_pipes_pos
                 self.upper_pipes_pos = [random.randint(self.SIDE_BOUND, self.game.RESOLUTION[0] - self.SIDE_BOUND), self.lower_pipes_pos[1]-self.PIPE_SPACING]
+                return True
+            return False
 
     class Blocks:
 
         SIZE = 35
         COLOR = (221, 166, 83)
+        MIN_BLOCK2BLOCK_DIST = 50
 
         def __init__(self, game):
             self.game = game
+            self.blocks = []
+            for l in range(4):
+                self.create_blocks(level=l)
+            self.new_block_buffer = True
 
-        def update(self, ofset):
-            pass
+        def create_blocks(self, level=2):
+            max_y = int(self.game.pipes.lower_pipes_pos[1] - self.game.pipes.PIPE_WIDTH - self.SIZE - self.game.pipes.PIPE_SPACING * level)
+            min_y = int(self.game.pipes.lower_pipes_pos[1] - self.game.pipes.PIPE_SPACING*(level+1) + self.game.pipes.PIPE_WIDTH + self.SIZE)
+
+            x1 = random.randint(self.SIZE, self.game.RESOLUTION[0] - self.SIZE)
+            y1 = random.randint(min_y, max_y)
+            block = [x1, y1]
+            self.blocks.append(block)
+
+            dist = self.MIN_BLOCK2BLOCK_DIST - 1
+            while dist < self.MIN_BLOCK2BLOCK_DIST:
+                x2 = random.randint(self.SIZE, self.game.RESOLUTION[0] - self.SIZE)
+                y2 = random.randint(min_y, max_y)
+                dist = sqrt(pow((x2-x1), 2) + pow((y2-y1), 2))
+
+            block = [x2, y2]
+            self.blocks.append(block)
+
+
+        def update(self, ofset, new_blocks=False):
+
+            for block in self.blocks:
+                block[1] += ofset
+                pg.draw.rect(self.game.screen, self.COLOR, (block[0]-self.SIZE//2, block[1]-self.SIZE//2, self.SIZE, self.SIZE))
+
+            if new_blocks:
+                if not self.new_block_buffer:
+                    self.create_blocks(2)
+                    self.blocks = self.blocks[2:]
+                if self.new_block_buffer: self.new_block_buffer = False
 
     RESOLUTION = (350, 500)
     FPS =  60
-    GRAVITY = 1.3
+    GRAVITY = 1.65
     BACKGROUND = (0, 136, 146)
     TOP_BOUND = 300
     HELPER_LINES = True
+    FONT = pg.font.SysFont('timesnewroman', 20)
 
     def __init__(self):
         self.screen = pg.display.set_mode(self.RESOLUTION)
@@ -123,35 +179,56 @@ class Game:
         self.ofset = 0
     
     def restart(self, players):
+        self.high_score = 0
         self.running = True
         self.pipes = self.Pipes(self)
         self.blocks = self.Blocks(self)
         self.players = [self.Player(self) for _ in range(players)]
+        self.best_player = self.players[0]
 
     def update(self):
         
         self.screen.fill(self.BACKGROUND)
 
-        self.pipes.update(self.ofset)
-        self.blocks.update(self.ofset)
+        new_pipe = self.pipes.update(self.ofset)
+        self.blocks.update(self.ofset, new_blocks=new_pipe)
 
+        if self.HELPER_LINES:
+            pg.draw.circle(self.screen, (255, 0, 0), self.blocks.blocks[3], 2)
+            pg.draw.circle(self.screen, (255, 0, 0), self.blocks.blocks[2], 2)
+            pg.draw.circle(self.screen, (255, 0, 0), self.blocks.blocks[1], 2)
+            pg.draw.circle(self.screen, (255, 0, 0), self.blocks.blocks[0], 2)
+
+        score = 0
         for player in self.players:
-            jump = 0
-            if is_pressed('a'):
-                jump = 1
-            if is_pressed('d'):
-                jump = 2
 
             max_ofset = 0
-            ofset = player.update(jump, self.ofset)
+            ofset = player.update(ofset=self.ofset, new_pipes=new_pipe)
             if ofset > max_ofset:
                 max_ofset = ofset
             self.ofset = max_ofset
-
-            if player.dead: self.restart(1)
         
-        if self.HELPER_LINES: pg.draw.line(self.screen, (255, 0, 0), (0, self.TOP_BOUND-self.players[0].SIZE//2), (self.RESOLUTION[0], self.TOP_BOUND-self.players[0].SIZE//2), 1)
+            if player.score > score: 
+                score = player.score
+                self.best_player = player
 
+        if score > self.high_score:
+            self.high_score = score
+
+        ########
+        # testing
+        if self.players[0].dead:
+            print(f'High Score: {self.high_score}')
+            self.restart(1)
+        ########
+
+        score_text = self.FONT.render(str(score), True, (255, 255, 255))
+        self.screen.blit(score_text, (0, 0))
+
+        if self.HELPER_LINES: 
+            pg.draw.line(self.screen, (0, 255, 255), (0, self.TOP_BOUND-self.players[0].SIZE//2), (self.RESOLUTION[0], self.TOP_BOUND-self.players[0].SIZE//2), 1)
+
+    def update_screen(self):
         for event in pg.event.get():
             if event.type == pg.QUIT:
                 self.running = False
@@ -166,4 +243,39 @@ game = Game()
 game.restart(1)
 
 while True:
+
     game.update()
+
+    #########
+    # AI HERE
+
+    below_block_left = [0, game.RESOLUTION[1]]
+    below_block_right = game.RESOLUTION
+    above_block_left = [0, 0]
+    above_block_right = [game.RESOLUTION[0], 0]
+
+    pg.draw.aaline(game.screen, (0, 0, 255), above_block_right, game.best_player.pos)
+    pg.draw.aaline(game.screen, (0, 0, 255), above_block_left, game.best_player.pos)
+    pg.draw.aaline(game.screen, (255, 0, 0), below_block_right, game.best_player.pos)
+    pg.draw.aaline(game.screen, (255, 0, 0), below_block_left, game.best_player.pos)
+
+    if game.pipes.lower_pipes_pos[1] < game.best_player.pos[1]:
+        above_pipes = game.pipes.lower_pipes_pos
+    else: above_pipes = game.pipes.upper_pipes_pos
+
+    pg.draw.aaline(game.screen, (0, 255, 0), above_pipes, game.best_player.pos)
+
+
+
+    for player in game.players:
+        jump = 0
+        if is_pressed('a'):
+            jump = 1
+        if is_pressed('d'):
+            jump = 2
+        player.set_jump(jump)
+
+    # AI HERE
+    #########
+
+    game.update_screen()
